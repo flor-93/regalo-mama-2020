3963b596bf24fb447c351fdfe8353d3191c7204b
Florencia Ferreyra Gre <javierc@mural.co>
2020-07-01 18:02:20 -0300

    React - Lección 3 - Ejercicio 3 (sin funcion)

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 //Lección 2 - Ejercicio 1
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root'),
-// );
+/*
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
-// ReactDOM.render(
-//   <div>
-//     <p>
-//       To get started, edit <code>src/App.js</code> and save to <b>reload</b>
-//     </p>
-//   </div>,
-//   document.getElementById('root'),
-// );
+ReactDOM.render(
+  <div>
+    <p>
+      To get started, edit <code>src/App.js</code> and save to <b>reload</b>
+    </p>
+  </div>,
+  document.getElementById('root'),
+);
+*/
 
 //Lección 2 - Ejercicio 2
+/*
 const element = (
   <div>
     <h1>Estudiá programación en Next-U</h1>
@@ -33,9 +36,11 @@ const element = (
     <a href="https://learn.nextu.com/">Next-U</a>
   </div>
 );
-// ReactDOM.render(element, document.getElementById('study'));
+ReactDOM.render(element, document.getElementById('study'));
+*/
 
 //Lección 2 - Ejercicio 3
+/*
 const form = (
   <form class="react-form">
     <fieldset>
@@ -58,9 +63,11 @@ const form = (
     </fieldset>
   </form>
 );
-// ReactDOM.render(form, document.getElementById('form'));
+ReactDOM.render(form, document.getElementById('form'));
+*/
 
 //Lección 3 - Ejercicio 1
+/*
 const img = React.createElement('img', {
   id: 'imagen',
   src: 'http://www.annarecetasfaciles.com/files/pan-casero-1024x575.jpg',
@@ -253,23 +260,130 @@ const subtittle3 = React.createElement(
 const containerPreparation = [subtittle2, listPreparations, subtittle3];
 
 ReactDOM.render(containerPreparation, document.getElementById('preparation'));
-
-// ReactDOM.render(header, document.getElementById('recipe'));
+*/
 
 //Lección 3 - Ejercicio 2
-/* 
 const title = React.createElement('div', { id: 'title' }, 'Pestañas con react');
 
-const tab1 = React.createElement('li', { id: 'tab-1' }, 'Física');
-const tab2 = React.createElement('li', { id: 'tab-2' }, 'Química');
-const tab3 = React.createElement('li', { id: 'tab-3' }, 'Biología');
-const tab4 = React.createElement('li', { id: 'tab-4' }, 'Inglés');
-const tab5 = React.createElement('li', { id: 'tab-5' }, 'Sociales');
-const tab6 = React.createElement('li', { id: 'tab-6' }, 'Historia');
-const tab7 = React.createElement('li', { id: 'tab-7' }, 'Deporte');
+ReactDOM.render(title, document.getElementById('container'));
 
-const pGeral = React.createElement('p', { id: '' });
-*/
+const createTabElement = React.createFactory('li');
+
+const tab1 = createTabElement({ class: 'tabs', type: 'none' }, 'Física');
+const tab2 = createTabElement({ class: 'tabs', type: 'none' }, 'Química');
+const tab3 = createTabElement({ class: 'tabs', type: 'none' }, 'Biología');
+const tab4 = createTabElement({ class: 'tabs', type: 'none' }, 'Inglés');
+const tab5 = createTabElement({ class: 'tabs', type: 'none' }, 'Sociales');
+const tab6 = createTabElement({ class: 'tabs', type: 'none' }, 'Historia');
+const tab7 = createTabElement({ class: 'tabs', type: 'none' }, 'Deporte');
+
+const tabs = [tab1, tab2, tab3, tab4, tab5, tab6, tab7];
+
+const listTabs = React.createElement('ul', { id: 'list-tabs' }, tabs);
+
+ReactDOM.render(listTabs, document.getElementById('tabs'));
+
+const createParagraphElement = React.createFactory('p');
+
+const pGral = createParagraphElement(
+  '',
+  'Ejemplo de pestañas creadas con elementos estáticos de React y CSS',
+);
+
+const pTab1 = createParagraphElement(
+  { class: 'info', id: 'fisica' },
+  'La física (del latín physica, y este del griego antiguo φυσικός, «natural, relativo a la naturaleza»)1​ es la ciencia natural que estudia los componentes fundamentales del Universo, la energía, la materia, el espacio-tiempo y las interacciones fundamentales.',
+);
+
+const pTab2 = createParagraphElement(
+  { class: 'info', id: 'quimica' },
+  'La química es la ciencia que estudia la composición, estructura y propiedades de la materia, así como los cambios que esta experimenta durante las reacciones químicas y su relación con la energía.',
+);
+
+const pTab3 = createParagraphElement(
+  { class: 'info', id: 'biologia' },
+  'La biología (del griego βίος [bíos] "vida", y -λογία [-logía] "tratado, estudio, ciencia",1​ que se connota como la "ciencia de la vida"2​) es la rama de la ciencia que estudia los procesos naturales3​ de los organismos vivos,4​ considerando su anatomía, fisiología, evolución, desarrollo, distribución y relaciones.',
+);
+
+const pTab4 = createParagraphElement(
+  { class: 'info', id: 'ingles' },
+  'El idioma inglés (English language o English, pronunciado /ˈɪŋɡlɪʃ/) es una lengua germánica occidental que surgió en los reinos anglosajones de Inglaterra y se extendió hasta el Norte en lo que se convertiría en el sudeste de Escocia, bajo la influencia del Reino de Northumbria.',
+);
+
+const pTab5 = createParagraphElement(
+  { class: 'info', id: 'sociales' },
+  'Las ciencias sociales son las ramas de la ciencia relacionadas con la sociedad y el comportamiento humano.',
+);
+
+const pTab6 = createParagraphElement(
+  { class: 'info', id: 'historia' },
+  'La historia es la ciencia que tiene como objetivo el estudio de sucesos del pasado, tradicionalmente de la humanidad1​, y como método, el propio de las ciencias sociales/humanas, así como el de las ciencias naturales en un marco de interdisciplinariedad.',
+);
+
+const pTab7 = createParagraphElement(
+  { class: 'info', id: 'deporte' },
+  'El deporte es una actividad reglamentada, normalmente de carácter competitivo y que puede mejorar la condición física2​ de quien lo practica, y además tiene propiedades que lo diferencian del juego.',
+);
+
+const createImgElement = React.createFactory('img');
+
+const img1 = createImgElement({
+  src:
+    'https://concepto.de/wp-content/uploads/2018/08/f%C3%ADsica-e1534938838719.jpg',
+  alt: 'fisica',
+  width: '500',
+});
+
+const img2 = createImgElement({
+  src: 'https://cienciadehoy.com/wp-content/uploads/2019/05/89676620.jpg',
+  alt: 'quimica',
+  width: '500',
+});
+
+const img3 = createImgElement({
+  src:
+    'https://www.ecestaticos.com/image/clipping/9714c7c06868f88fba1f532ee53e155d/nutrigenetica-la-biologia-molecular-en-el-campo-de-la-nutricion.jpg',
+  alt: 'biologia',
+  width: '500',
+});
+
+const img4 = createImgElement({
+  src:
+    'https://imagenes.universia.net/gc/net/images/educacion/p/pr/pro/professores-ingles-concorrer-bolsa-estudar-eua-noticias.jpg',
+  alt: 'ingles',
+  width: '500',
+});
+
+const img5 = createImgElement({
+  src:
+    'https://upload.wikimedia.org/wikipedia/commons/6/63/GlobalSocial_850.jpg',
+  alt: 'sociales',
+  width: '500',
+});
+
+const img6 = createImgElement({
+  src:
+    'https://www.ecestaticos.com/imagestatic/clipping/af8/705/af87059eaa01d0ce789779b415c78134/como-se-debe-ensenar-la-historia.jpg?mtime=1579565836',
+  alt: 'historia',
+  width: '500',
+});
+
+const img7 = createImgElement({
+  src:
+    'https://s.france24.com/media/display/6aca8d1a-7783-11ea-9cf2-005056bf87d6/w:1280/p:16x9/WEB%2005ABR%20DEPORTES%20PORTADA%20FOTO.webp',
+  alt: 'deporte',
+  width: '500',
+});
+
+const physical = [pGral, pTab1, img1];
+const chemistry = [pGral, pTab2, img2];
+const biology = [pGral, pTab3, img3];
+const english = [pGral, pTab3, img4];
+const social = [pGral, pTab3, img5];
+const history = [pGral, pTab6, img6];
+const sport = [pGral, pTab7, img7];
+
+ReactDOM.render(physical, document.querySelector('#info'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
