c97ebfec69740d39e8f40fe15a9af5bbb9fe93ee
Florencia Ferreyra Gre <flor.ferreyragre@gmail.com>
2020-07-06 19:52:52 -0300

    Prueba 1

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './index.css';
 
 class ReactClass extends Component {
   render() {
     return (
-      <div id="form">
-        <h1 id="header">Ingreso</h1>
-
-        <div id="field">
-          <div id="cnt1" className="cnt">
-            <i className="fa fa-envelope input-group input-group-prepend input-group-text icon" />
-            <input
-              type="email"
-              className="inp input-group input-group-prepend input-group-text form-control"
-              name="mail"
-              placeholder="Correo Electrónico"
+      <div className="cnt">
+        <Carousel>
+          <div className="img">
+            <img
+              alt="Harry Potter 1"
+              src="https://www.cerodosbe.com/uploads/s1/10/04/73/56/el-universo-harry-potter-aterriza-a-lo-grande-en-nueva-york_15_970x597.jpeg"
             />
+            <p className="legend">Legend 1</p>
           </div>
-
-          <div id="cnt2" className="cnt">
-            <i className="fa fa-key input-group input-group-prepend input-group-text icon" />
-            <input
-              type="password"
-              className="inp inp input-group input-group-prepend input-group-text form-control"
-              name="contraseña"
-              placeholder="Contraseña"
+          <div className="img">
+            <img
+              alt="Harry Potter 2"
+              src="https://cdn.pocket-lint.com/r/s/970x/assets/images/150401-tv-feature-harry-potter-image1-vpdnsqfrou-jpg.webp?v1"
             />
+            <p className="legend">Legend 2</p>
           </div>
-        </div>
+        </Carousel>
       </div>
     );
   }
 }
-ReactDOM.render(<ReactClass />, document.body);
+
+ReactDOM.render(<ReactClass />, document.querySelector('#content'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
